d887b33f180c67718cc96e76a8274b73eb066567
Mark Devereux <MarkBDevereux@gmail.com>
2021-10-14 10:16:48 +0800

    Popup Card

--- client/src/PopUpCard.jsx
+++ client/src/PopUpCard.jsx
@@ -11,19 +11,20 @@ function PopUpCard(props)
     const rate = props.rate
     const amount = props.amount
     
-    return(
-        <div className = 'container column-tasks'>
-            {props.showPopUp ? <div>
-            <h4>Title: {title}</h4>
-            <p>Task Type: {type}</p>
-            <p>Info: {description}</p>
-            <p>Suburb: {suburb}</p>
-            <p>Date: {date}</p>
-            <p>Rate {rate}</p>
-            <p>Amount: {amount}</p>
-            </div> : null}         
-        </div>
-    )
+    return (props.showPopUp) ? (
+        <div className = 'popup'>
+            <div className = 'popup-inner'>            
+                <h2>{title}</h2>
+                <h4>{type}</h4>
+                <h4>{description}</h4>
+                <h4>{suburb}</h4>
+                <h4>{date}</h4>
+                <h4>{rate}</h4>
+                <h4>{amount}</h4>
+                <button className = 'close-button' onClick = {() => props.setShowPopUp(false)}>x</button>
+            </div>        
+        </div>) : '';       
+    
 }
 
 export default PopUpCard;
